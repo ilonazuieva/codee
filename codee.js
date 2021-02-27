d6ef6e25399b8283eab94f66acba32d0fb2e8675
ilona <isaevaangelina@gmail.com>
2021-02-28 00:11:18 +0200

    Initial commit

---
+++ codee.js
@@ -0,0 +1,12 @@
+function padIt(str,n){
+    while(n > 0){
+        if(n % 2 === 0) {
+            str = str + "*";
+        }else{
+            str = "*" + str;
+        }
+        n --;
+    }
+    return str;
+}
+//https://www.codewars.com/kata/57216d4bcdd71175d6000560
